0afc40a18672dd12ea91b1cbdc768ffa9f67001f
virat singh <virat.singh@airbnb.com>
2017-03-07 20:45:09 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by virat_singh on 3/7/17.
+//  Copyright © 2017 viratsingh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
